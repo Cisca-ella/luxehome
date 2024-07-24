d4a540249e33533243e5a75634a66a6b4d4be252
JayMidas001 <nwokoella@gmail.com>
2024-07-24 17:54:02 +0100

    created a shoppincart page

---
+++ src/ShoppingCart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './ShoppingCart.css';
+
+export default function ShoppingCart() {
+    return(
+        <div className='parent-div'>
+           <div className='shop-div'>
+            
+            </div> 
+            <div className='order-div'>
+            
+            </div>
+        </div>
+    );
+    
+}
